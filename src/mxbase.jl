81adb20890c5fde68d76a7f94ac82f9c1d755103
Simon Kornblith <simon@simonster.com>
2013-04-04 20:40:32 -0400

    Automatically locate MATLAB and MATLAB libraries on OS X and Linux
    This patch allows MATLAB.jl to be used on most OS X and Linux systems
    without setting environmental variables.
    
    On Linux, the location of the MATLAB home directory is inferred from
    the path to the matlab executable. On OS X, the Applications directory
    is searched and the newest MATLAB version is used. The MATLAB_HOME
    environmental variable may be used to override the default location.
    
    Additionally, this patch provides the full path to libmx and libeng to
    dlopen(). On the Linux and OS X systems I tested on, this eliminates
    the need to set LD_LIBRARY_PATH or DYLD_FALLBACK_LIBRARY_PATH.

--- src/mxbase.jl
+++ src/mxbase.jl
@@ -2,12 +2,59 @@
 libmx = C_NULL
 libeng = C_NULL
 
+# Determine MATLAB library path and provide facilities to load libraries with
+# this path
+
+function get_paths()
+    global matlab_homepath = get(ENV, "MATLAB_HOME", "")
+    global default_startcmd = C_NULL
+    global matlab_library_path = nothing
+
+    if matlab_homepath == ""
+        if OS_NAME == :Linux
+            matlab_homepath = dirname(dirname(realpath(chomp(readall(`which matlab`)))))
+        elseif OS_NAME == :Darwin
+            apps = readdir("/Applications")
+            apps = apps[Bool[ismatch(r"^MATLAB_R[0-9]+[ab]\.app$", app) for app in apps]]
+            if ~isempty(apps)
+                matlab_homepath = joinpath("/Applications", min(apps))
+            end
+        end
+    end
+
+    if matlab_homepath == ""
+        error("The MATLAB path could be found. Set the MATLAB_HOME environmental variable to specify the MATLAB path.")
+    end
+
+    default_startcmd = joinpath(matlab_homepath, "bin", "matlab")
+    if !isexecutable(default_startcmd)
+        error("The MATLAB path is invalid. Set the MATLAB_HOME evironmental variable to the MATLAB root.")
+    end
+
+    # Get path to MATLAB libraries
+    matlab_library_path = nothing
+    if OS_NAME == :Linux
+        matlab_library_path = joinpath(matlab_homepath, "bin", (Int == Int32 ? "glnx86" : "glnxa64"))
+    elseif OS_NAME == :Darwin
+        matlab_library_path = joinpath(matlab_homepath, "bin", (Int == Int32 ? "maci" : "maci64"))
+    end
+
+    if matlab_library_path != nothing && !isdir(matlab_library_path)
+        matlab_library_path = nothing
+    end
+end
+get_paths()
+
+matlab_library(lib::String) =
+    matlab_library_path == nothing ? lib : joinpath(matlab_library_path, lib)
+
 # libmx (loaded when the module is imported)
 
 function load_libmx()
     global libmx
     if libmx == C_NULL
-        libmx = dlopen("libmx", RTLD_GLOBAL | RTLD_LAZY)
+        libmx = dlopen(matlab_library("libmx"), RTLD_GLOBAL | RTLD_LAZY)
+
         if libmx == C_NULL
             error("Failed to load libmx.")
         end
@@ -18,28 +65,10 @@ load_libmx()
 
 # libeng (loaded when needed)
 
-function get_default_startcmd()
-    if OS_NAME == :Darwin
-        matlab_homepath = get(ENV, "MATLAB_HOME", "")
-        if isempty(matlab_homepath)
-            error("The environment variable MATLAB_HOME must be set to specify the MATLAB path.")
-        end
-        binfile = joinpath(matlab_homepath, "bin/matlab")
-        if !isfile(binfile)
-            error("$(matlab_homepath) seems not a valid MATLAB home path.")
-        end
-        binfile
-    else
-        C_NULL
-    end
-end
-
-const default_startcmd = get_default_startcmd()
-
 function load_libeng()
     global libeng
     if libeng == C_NULL
-        libeng = dlopen("libeng", RTLD_GLOBAL | RTLD_LAZY)
+        libeng = dlopen(matlab_library("libeng"), RTLD_GLOBAL | RTLD_LAZY)
         if libeng == C_NULL
             error("Failed to load libeng.")
         end
