449f5d8c0b3090080073fee860f129153295d905
Simon Kornblith <simon@simonster.com>
2013-05-10 16:32:26 -0400

    Remove errant space

--- src/mstatements.jl
+++ src/mstatements.jl
@@ -118,7 +118,7 @@ function write_mstatement(io::IO, ex::Expr)
         write_mstatement(io, a[na])
         print(io, "}")
 
-    elseif h == :. 
+    elseif h == :.
         write_mstatement(io, a[1])
         print(io, ".")
         print(io, string(eval(a[2])))
