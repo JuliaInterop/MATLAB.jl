bb6137e429ffebed1083922a337d938d1a9e4069
Mustafa Mohamad <mus-m@outlook.com>
2021-04-28 23:32:34 -0400

    Test MATLAB CI?

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,11 +1,7 @@
 using MATLAB
 using Test
 
-is_ci() = get(ENV, "CI", nothing) == "true"
-
-if !is_ci() # only test if not CI
 include("engine.jl")
 include("matfile.jl")
 include("matstr.jl")
 include("mxarray.jl")
-end
